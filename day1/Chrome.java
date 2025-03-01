bc832639e192e291080ab376a7385968902f12ce
murugavelavan <murugaveal.rpm@gmail.com>
2025-03-02 04:25:29 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version =91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseyear = 2008;
+		char shortcutkey = 'C';
+		System.out.println("Browser Details"+" "+ "Browser Version - "+ version + " " +
+				"Developed By - "+ developer + " " + "Release Type - "+ isBeta + " " + 
+				"Release Year - "+ releaseyear +" "+ "Shortcut Key - "+ shortcutkey);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f; 
+				String browserName = "firefox" ;
+				 boolean isVisible = true ;
+				int releaseYear = 1998 ;
+				 char browserLogo = 'f' ;
+System.out.println("browserName is :"+browserName+" "+"browserVersion is :" + browserVersion +" "+ "releaseYear is : "  + releaseYear+" "+"browserLogo is : "+browserLogo +" " + "isVisible "+ isVisible );
+	}
+
+}
